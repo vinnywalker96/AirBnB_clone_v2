12cfa03e1855bae76f54b0773704f4b137ec9b8c
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-12 14:19:18 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -41,7 +41,7 @@ printf %s "server{
 	index index.html index.htm;
 
 	location /hbnb_static {
-		root /data/web_static/current/;
+		root /data/web_static/current/hbnb_static;
 		index index.html index.htm;
 	}
 
