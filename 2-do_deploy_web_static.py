2e716c92cca9cea3b0abcbebdaebb80d4d2317d3
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-14 16:14:22 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,9 +5,8 @@ from fabric.api import run
 import os
 
 
-env.hosts = ['35.153.93.29', '54.146.57.141']
+env.hosts = ['18.233.66.135', '18.207.141.188']
 env.user = 'ubuntu'
-env.key_filename = '~/ssh/id_rsa'
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
@@ -18,20 +17,22 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if not os.path.isfile(archive_path):
+    if not os.path.exists(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0] 
     status = True
+
     try:
-        with cd('/tmp/'):
-            put(archive_path)
-        sudo('mkdir -p /data/web_static/release/{}'.format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_path, name))
-        run('rm -rf /tmp/{}'.format(archive_path))
-        sudo('rm -rf /data/web_static/current')
-        sudo('ln -s /data/web_static/releases/{} /data/web_static/current'.format(name))
+        tgz_file = os.path.basename(archive_path)
+        put(archive_path, "/tmp/{}".format(tgz_file))
+        dir_name = tgz_file.replace('.tgz', '')      
+        run("sudo mkdir -p /data/web_static/releases/{}".format(dir_name))
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(tgz_file, dir_name))
+        run("sudo rm -rf /tmp/{}".format(tgz_file))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{} /data/web_static/current".format(dir_name))
+        print("New version deployed!")
         return status
-    except Exception as e:
+    except Exception as err:
         status = False
-        return status 
+        return status
+        
