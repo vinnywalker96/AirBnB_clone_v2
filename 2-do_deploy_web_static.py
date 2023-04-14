922747164cea3c1419d1f3fcb1f3ef2f102980a5
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-14 16:34:55 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,6 +2,7 @@
 # Fabfile to distribute an archive to a web server.
 from fabric.api import env
 from fabric.api import run
+from fabric.api import put
 import os
 
 
@@ -19,20 +20,19 @@ def do_deploy(archive_path):
     """
     if not os.path.exists(archive_path):
         return False
-    status = True
+   
 
     try:
         tgz_file = os.path.basename(archive_path)
         put(archive_path, "/tmp/{}".format(tgz_file))
         dir_name = tgz_file.replace('.tgz', '')      
-        run("sudo mkdir -p /data/web_static/releases/{}".format(dir_name))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(tgz_file, dir_name))
-        run("sudo rm -rf /tmp/{}".format(tgz_file))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{} /data/web_static/current".format(dir_name))
+        run("mkdir -p /data/web_static/releases/{}".format(dir_name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(tgz_file, dir_name))
+        run("rm -rf /tmp/{}".format(tgz_file))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{} /data/web_static/current".format(dir_name))
         print("New version deployed!")
-        return status
+        return True
     except Exception as err:
-        status = False
-        return status
+        return False
         
