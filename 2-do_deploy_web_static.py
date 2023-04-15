1774b4d0839f3dd7e865f00fe854656caa609b63
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-15 11:25:55 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -30,6 +30,9 @@ def do_deploy(archive_path):
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(tgz_file, dir_name))
         run("rm -rf /tmp/{}".format(tgz_file))
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/"
+            .format(dir_name, dir_name))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{} /data/web_static/current"
             .format(dir_name))
