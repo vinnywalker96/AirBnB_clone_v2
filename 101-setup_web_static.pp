51285e6b680b57db31c355d9b95859fbacd68a3a
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-17 09:53:09 +0000

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -14,6 +14,18 @@ class nginx_server {
    file { '/data':
        ensure => directory,
    }
+   file { '/data/web_static':
+       ensure => directory,
+   }
+   file { '/data/web_static/releases':
+        ensure => directory,
+   }
+   file { '/data/web_static/releases/test':
+        ensure => directory,
+   }
+   file { '/data/web_static/shared':
+        ensure => directory,
+   }
    exec { 'chown -R ubuntu /data/':
         path => '/usr/bin/:/usr/local/bin/:bin/'
    }
