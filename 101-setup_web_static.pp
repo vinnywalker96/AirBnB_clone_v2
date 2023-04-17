bb28752cfbf1c55a7b913102a4ad49be30f52811
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-17 06:44:53 +0000

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,52 +1,72 @@
-# sets up web servers for the deployment of web_static
+# sets up your web servers for the deployment of web_static. 
 
-# Check if Nginx in installed
-if ! package{'nginx':
-    ensure => installed,
-} {
-    service {'nginx':
-        ensure => running,
-        enable => true,
-    }
+#updates ubuntu server
+exec {'update server':
+    command  => 'apt-get update',
+    user     => 'root',
+    provider => 'shell',
 }
-
-# Checks if directory path exist and creates
-file {'/data/web_static/releases/test/':
+->
+# install nginx web server
+package { 'nginx':
+    ensure   => present,
+    provider => 'apt',
+}
+->
+file { '/data/web_static/shared':
     ensure => directory,
-}
-file {'/data/web_static/shared/':
-    ensure => directory,
-}
-file {'/data/web_static/releases/test/index.html':
-    ensure  => present,
-    content => '<html>\n    <head>\n    </head>\n    <body>\n         Holberton School\n    </body>\n</html>\n',
-}
-
-$SOURCE_DIR = '/data/web_static/current'
-$TARGET_DIR = '/data/web_static/releases/test/'
-
-# Creates a new link
-file {'$SOURCE_DIR':
-    ensure => link,
-    target => "$TARGET_DIR",
-}
-
-# Give ownership to ubuntu user
-file {'/data/':
     owner => 'ubuntu',
     group => 'ubuntu',
-    recurse => true,
+    mode => '0755',
 }
-
-file {'/etc/nginx/sites-available/default':
-    ensure  => present,
-    content => "server{\n 	listen 80 default_server;\n	listen [::]:80 default_server;\n	add_header X-Served-By $HOSTNAME;\n	server_name vestec.tech;\n	root /var/www/html;\n	index index.html index.htm;\n\n	location /hbnb_static {\n		alias  /data/web_static/current/;\n		index index.html index.htm;\n		\n	}\n\n	location /redirect_me {\n		return 301 https://github.com/vinnywalker96;\n	}\n	error_page 404 /404.html;\n        location = /404.html{\n             internal;\n         }\n}",
+->
+file { '/data/web_static/releases/test/':
+    ensure => directory,
+    owner => 'ubuntu',
+    group => 'ubuntu,
+    mode => '0755',
 }
+->
+file { '/data/web_static/releases/test/index.html':
+    content => 'Holberton School',
+    require => Package['nginx'],
+}
+-> 
+file { '/datta/web_static/current':
+    ensure => link,
+    target => '/data/web_static/releases/test/',
+    force => true,
+}
+->
+file { '/etc/nginx/sites-available/default':
+    content => "\
+    server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	add_header X-Served-By $HOSTNAME;
+	server_name vestec.tech;
+	root /var/www/html;
+	
+	location /hbnb_static {
+		alias /data/web_static/current/;
+		index index.html index.htm;
+	}
 
-service {'nginx':
-    ensure => running,
+	location /redirect_me {
+		return 301 https://github.com/vinnywalker96;
+	}
+
+	error_page 404 /404.html;
+	location = /404.html{
+		internal;
+	}
+    }",
+    require => Package['nginx'],
+    notify => Servive['nginx'],
+}
+->
+service { 'nginx':
+    ensure => 'running',
     enable => true,
-    subscribe => File['/etc/nginx/sites-available/default'],
+    require => Package['nginx'],
 }
-
-
