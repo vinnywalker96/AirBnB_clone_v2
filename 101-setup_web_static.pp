f9d11df6650c89170d0d91f80dbb47a775aaaf49
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-17 09:18:11 +0000

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,73 +1,24 @@
-# Configures a web server for deployment of web_static.
+# Configures a web server for deployment of web_static
 
-package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt'
-} ->
+class nginx_server {
+   package { 'nginx':
+       ensure => installed,
+   }
 
-file { '/data':
-  ensure  => 'directory'
-} ->
+   service { 'nginx':
+       ensure => running,
+       enable => true,
+       require => Package['nginx'],
+   }
 
-file { '/data/web_static':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases/test':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/shared':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  content => "Holberton School Puppet\n"
-} ->
-
-file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test'
-} ->
-
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+   file { '/data/web_static/releases/':
+       ensure => directory,
+       mode => '0755',
+       owner => 'ubuntu',
+       group => 'ubuntu',
+       recurse => true,
+   }
 }
 
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => "\
-  server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	add_header X-Served-By $HOSTNAME;
-	server_name vestec.tech;
-	root /var/www/html;
-	
-	location /hbnb_static {
-		alias /data/web_static/current/;
-		index index.html index.htm;
-	}
-
-	location /redirect_me {
-		return 301 https://github.com/vinnywalker96;
-	}
-
-	error_page 404 /404.html;
-	location = /404.html{
-		internal;
-	}
-    }",
-    require => Package['nginx'],
-    notify => Servive['nginx'],
-
-} ->
-
-exec { 'nginx restart':
-  path => '/etc/init.d/'
-}
+include nginx_server
+  
