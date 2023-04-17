0ab567190dc021d37a4881bf9f57c10e0c4d7ce4
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-17 10:48:35 +0000

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -11,13 +11,26 @@ class nginx_server {
        require => Package['nginx'],
    }
 
-    exec { 'mkdir -p /data/web_static/releases/test/':
-       path => '/usr/bin/:/usr/local/bin/:/bin/'
+   file { '/data/':
+       ensure => directory,
    }
 
-   exec { 'mkdir -p /data/web_static/shared/':
-        path => '/usr/bin/:/usr/local/bin/:/bin/'
+   file { '/data/web_static/':
+       ensure => directory,
    }
+ 
+   file { '/data/web_static/releases/':
+       ensure => directory,
+   }
+
+   file { '/data/web_static/shared/':
+       ensure => directory,
+   }
+
+   file { '/data/web_static/releases/test/':
+       ensure => directory,
+   }
+
 
    file { '/data/web_static/releases/test/index.html':
         content => 'Holberton School',
@@ -36,6 +49,31 @@ class nginx_server {
        ensure => link,
        target => '/data/web_static/releases/test'
    }
+
+  file { '/etc/nginx/sites-available/default':
+      ensure  => 'present',
+      content => "\
+ server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    if (\$request_filename ~ redirect_me) {
+        rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    }
+}",
+    }
+    exec { 'nginx restart':
+        path => '/etc/init.d/'
+    }
+
+
 }
 
 include nginx_server
