369b3b31590baf374ea2bd1620c44977ff927837
Marlvin Kwenda <vinnywalker96@gmail.com>
2023-05-05 15:37:00 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
-sudo mkdir -p /data/web_static/releases/test/ 
+sudo mkdir -p /data/ 
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo ln /data/web_static/current /data/web_static/releases/test/
+sudo ln -s  /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu /data/
 sudo chgrp -R ubuntu /data/
