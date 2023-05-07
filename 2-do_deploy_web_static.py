0b8fadc6521ab335d140e5d91c8b3abf992ef9e8
Marlvin Kwenda <vinnywalker96@gmail.com>
2023-05-07 15:24:37 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,11 +2,28 @@
 from fabric.api import put 
 from fabric.api import env
 from fabric.api import run
+from fabric.api import sudo
+from datetime import datetime
 import os
 
-env.user = 'ubuntu'
 env.hosts = ['54.144.46.42', '100.25.194.141']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the webstatic
+    """
+    try:
+        if not os.path.exists("versions"):
+            os,mkdir("versions")
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = "versions/web_static_{}.tgz".format(current_time)
+        local("tar -czvf {} web_static".format(filename))
+        return filename
+    except:
+        return None
 
 def do_deploy(archive_path):
     """
@@ -14,27 +31,20 @@ def do_deploy(archive_path):
     """
     if not os.path.exists(archive_path):
         return False
-
     try:
-        archive_filename = os.path.basename(archive_path)
-        put(archive_path, "/tmp/{}".format(archive_filename))
-        archive_folder = archive_filename.replace('.tgz', '')
-        run("sudo mkdir -p /data/web_static/releases/{}"
-            .format(archive_folder))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}"
-            .format(archive_filename, archive_folder))
-        run("sudo rm -rf /tmp/{}".format(archive_file))
-        run("sudo mv /data/web_static/releases/{}/web_static/* "\
-            "/data/web_static/releases/{}"
-            .format(archive_folder, archive_folder))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static/"
-            .format(archive_folder))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{} "\
-            "/data/web_static/current".format(archive_folder))
+        put(archive_path, "/tmp/")
+        filename = os.path.basename(archive_path)
+        foldername = "/data/web_static/releases/" + os.path.splittext(filename)[0]
+        run("mkdir -p {}".format(foldername))
+        run("tar -xzf /tmp/{} -C {} -- strip-components=1".format(filename, foldername))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}/web_static/* {}".format(foldername))
+        run("rm -rf {}/web_static".format(foldername))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(foldername))
         print("New version deployed!")
         return True
     except:
-        return False
-        
+       return False
 
+ 
