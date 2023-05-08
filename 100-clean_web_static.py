49b4fd675b43f6061d7c158905cc494841a5f19e
Marlvin Kwenda <vinnywalker96@gmail.com>
2023-05-08 11:08:56 +0000

    Keep it clean

---
+++ 100-clean_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from fabric.api import local
+from fabric.api import run
+from fabric.api import env
+from fabric.context_managers import cd
+from fabric.context_managers import lcd
+import os
+
+
+env.hosts = ["54.144.46.42", "100.25.194.141"]
+env.user = "ubuntu"
+
+
+def do_clean(number=0):
+    """deletes out-of-date archives"""
+    number = int(number)
+    if number == 0 or number == 1:
+        number = 1
+    else:
+        number += 1
+    with lcd('versions'):
+        local('ls -1t | tail -n +{} | xargs rm -rf --'
+              .format(number))
+    with cd('/data/web_static/releases'):
+        run('ls -1t | tail -n +{} | xargs rm -rf --'
+            .format(number))
