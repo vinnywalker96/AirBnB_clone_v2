85520c0ddf591cddb89d3d3c72fe41a5db603368
Marlvin Kwenda <vinnywalker96@gmail.com>
2023-05-08 07:46:00 +0000

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,36 +4,29 @@ from fabric.api import put
 from fabric.api import run
 import os
 
-env.hosts = ["54.144.46.42", "100.25.194.141"]
-env.user = "ubuntu"
+env.hosts = ["100.25.194.141", "54.144.46.42"]
+
 
 def do_deploy(archive_path):
-    """Distributes an archive to web servers"""
+    """
+    distributes an archive to your web servers
+    """
     if not os.path.exists(archive_path):
         return False
-
     try:
-        # Upload archive to /tmp/ directory of web server
-        archive_filename = os.path.basename(archive_path)
-        put(archive_path, '/tmp/{}'.format(archive_filename))
-
-        # Uncompress archive to /data/web_static/releases/<archive filename without extension> on web server
-        archive_folder = archive_filename.replace('.tgz', '')
-        run('mkdir -p /data/web_static/releases/{}'.format(archive_folder))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(archive_filename, archive_folder))
-
-        # Delete archive from web server
-        run('rm /tmp/{}'.format(archive_filename))
-
-        # Delete symbolic link /data/web_static/current from web server
-        run('rm -rf /data/web_static/current')
-
-        # Create new symbolic link /data/web_static/current on web server
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(archive_folder))
-
+        filename = os.path.base(archive_path)
+        foldername = filename.split(".")[0]
+        path = "/data/web_static/releases/{}".format(foldername)
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path))
+        run("tar -xzf /tmp/{} -C {}/".format(filename, path))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}/web_static/* {}".format(path, path))
+        run("rm -rf {}/web_static".format(path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(path))
+        print("New version deployed!")
         return True
     except:
         return False
-
+        
