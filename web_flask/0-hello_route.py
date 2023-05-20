c18f7a1703d21a4cb302af1de4333c630a842893
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-20 11:55:22 +0000

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """displays 'Hello HBNB'"""
-    return "Hello HBNB!"
+    """Returns 'Hello HBNB'"""
+    return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
