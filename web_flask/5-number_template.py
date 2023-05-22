b61721f8b68eef77a65cabcc44a79256f2d61569
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-22 10:37:25 +0000

    Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Starts a Flask web App
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+    /hbnb: Displays 'HBNB'
+    /c/<text>: display 'C' followed by the symbol
+"""
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Returns 'Hello HBNB'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<path:text>', strict_slashes=False)
+def display_c(text):
+    """Returns C (followed by text)"""
+    sub_path = ""
+    for sub in text:
+        if sub == "_":
+            sub_path += " "
+            continue
+        sub_path += sub
+    text = sub_path
+    return 'C {}'.format(escape(text))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def show_python(text="is cool"):
+    """Returns Python (followed by text)"""
+    sub_path = ""
+    for sub in text:
+        if sub == "_":
+            sub_path += " "
+            continue
+        sub_path += sub
+    text = sub_path
+    return "Python {}".format(escape(text))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def show_n(n):
+    """Return n is number"""
+    return "{} is a number".format(escape(n))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Return Number: n"""
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
