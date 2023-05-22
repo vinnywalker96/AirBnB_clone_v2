518fb3089bafa09c9411d5b9d7f74e51b46db20a
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-22 08:17:02 +0000

    Is it a number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -52,11 +52,5 @@ def show_python(text="is cool"):
     return "Python {}".format(escape(text))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def show_number(n):
-    """Return Number passed"""
-    return "{}".format(escape(n))
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
