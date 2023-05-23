1e8dead2ab45b8af276262d34cfbcfbad11a2ee5
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-23 12:40:29 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,19 +1,25 @@
+#!/usr/bin/python3
+"""Starts a Flask web App
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+        the list of all State objects present in DBStorage sorted by name
+"""
 from flask import Flask, render_template
 from models import storage, State
 
 app = Flask(__name__)
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Retrieves state in db"""
+    """Return Number: n"""
     states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda  state: state.name)
-    return render_template("7-states_list.html", states=sorted_states);
- 
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', state=sorted_states)
 
 @app.teardown_appcontext
-def close_session(exception=None):
-    """Closes database session"""
+def close_session():
+    """Close database connection"""
     storage.close()
 
 
