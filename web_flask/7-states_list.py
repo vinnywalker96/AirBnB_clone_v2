f35b0ec93dc0da73480f2a5af2aa62dbd55145be
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-24 22:11:25 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,12 +10,14 @@ from models import storage
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Return Number: n"""
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', state=sorted_states)
+    return render_template('7-states_list.html', states=sorted_states)
+
 
 @app.teardown_appcontext
 def close_session(exception=None):
@@ -24,4 +26,4 @@ def close_session(exception=None):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
